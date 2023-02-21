2ee34f459297014cea2fcc10f100453c1a6073d8
Mikhail Nazaretyan <misha@mlsw.com>
2023-02-21 09:36:36 +0100

    Add an .env file with the BOT_KEY token

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const { Telegraf } = require("telegraf");
+require("dotenv").config();
+
+const bot = new Telegraf(process.env.BOT_KEY);
+
+bot.command("test", (ctx) => {
+  console.log("test command deployed");
+  ctx.reply("test command deployed");
+});
+
+bot.launch();
+console.log(`bot is running`);
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
